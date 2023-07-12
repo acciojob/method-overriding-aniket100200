339c850c2c386a3e0502f2ce062c289fe61abdb8
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-07-12 10:11:15 +0530

    Method Overriding is done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,32 @@
 package com.driver;
 
-public class Main {
-  
+import java.sql.SQLOutput;
+import java.util.spi.AbstractResourceBundleProvider;
+
+public class Main
+{
+   static class A{
+       public A(){}
+
+       public String meth()
+       {
+           return "Invoking method from class A";
+       }
+   }
+
+   static  class B extends A{
+       @Override
+       public String meth()
+       {
+           super.meth();
+           return "Method is overridden in Extendend class B";
+       }
+   }
+
+   public static void main(String[]args)
+   {
+       B obj=new B();
+       obj.meth();
+
+   }
 }
